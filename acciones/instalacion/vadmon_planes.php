f49149fc656962ad2661575e993e84d1f6ad3232
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-27 17:20:02 -0500

    Updated vadmon_planes.php

--- acciones/instalacion/vadmon_planes.php
+++ acciones/instalacion/vadmon_planes.php
@@ -2,12 +2,14 @@
 // Creamos la tabla
 $strCreatePlanesTableSQL = 'CREATE TABLE vadmon_planes (';
 $strCreatePlanesTableSQL .= 'id SERIAL PRIMARY KEY, usuario VARCHAR(30) NOT NULL, contrasenia VARCHAR(32) NOT NULL, planusuarios BOOLEAN DEFAULT FALSE, planpermisos BOOLEAN DEFAULT FALSE,';
-$strCreatePlanesTableSQL .= 'planarticulos BOOLEAN DEFAULT FALSE, plannoticias BOOLEAN DEFAULT FALSE, planencuestas BOOLEAN FALSE, planpromociones BOOLEAN DEFAULT FALSE,';
+$strCreatePlanesTableSQL .= 'planarticulos BOOLEAN DEFAULT FALSE, plannoticias BOOLEAN DEFAULT FALSE, planencuestas BOOLEAN DEFAULT FALSE, planpromociones BOOLEAN DEFAULT FALSE,';
 $strCreatePlanesTableSQL .= 'plandisenio BOOLEAN DEFAULT FALSE, planregistro BOOLEAN DEFAULT FALSE, servidor VARCHAR(255) NOT NULL, servidordb VARCHAR(255) NOT NULL,';
 $strCreatePlanesTableSQL .= 'servidorusuario VARCHAR(255) NOT NULL, servidorpass VARCHAR(255) NOT NULL, servidorftpdirectorio VARCHAR(255) NOT NULL,';
 $strCreatePlanesTableSQL .= 'servidorftpusuario VARCHAR(255) NOT NULL, servidorftppass VARCHAR(255) NOT NULL, activo BOOLEAN DEFAULT TRUE';
 $strCreatePlanesTableSQL .= ');';
-echo $strCreatePlanesTableSQL;
-$rslt = pg_query($planconexion, $strCreatePlanesTableSQL);
-echo $rslt;
+$strCreatePlanesTableSQLName = 'createPlanes';
+if(pg_prepare($planconexion, $strCreatePlanesTableSQLName, $strCreatePlanesTableSQL))
+{
+	$result = pg_execute($planconexion, $strCreatePlanesTableSQLName);
+}
 ?>
