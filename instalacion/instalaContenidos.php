8a9baa432695e28d29e33464163dcbac6225180e
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-27 13:08:42 -0500

    Updated instalaContenidos.php

--- instalacion/instalaContenidos.php
+++ instalacion/instalaContenidos.php
@@ -10,26 +10,12 @@ $strPgConnection = 'dbname='.$baseDatos.' host='.$servidor.' port=5432 ';
 $strPgConnection .= 'user='.$usuarioBase.' password='.$passBase.' sslmode=require';
 $conexion = pg_connect($strPgConnection);
 
-$result = pg_query($conexion, "CREATE TABLE `vadmon_contenidos` (
-  `id` int(11) NOT NULL auto_increment,
-  `subcontenido` varchar(255) collate ".$cotejamiento." default '0',
-  `menucontenido` varchar(255) collate ".$cotejamiento." default NULL,
-  `ordencontenido` int(11) default '0',
-  `titulocontenido` varchar(255) collate ".$cotejamiento." default NULL,
-  `textocontenido` text collate ".$cotejamiento.",
-  `imagen1contenido` varchar(255) collate ".$cotejamiento." default NULL,
-  `imagen2contenido` varchar(255) collate ".$cotejamiento." default NULL,
-  `imagen3contenido` varchar(255) collate ".$cotejamiento." default NULL,
-  `creador` varchar(255) collate ".$cotejamiento." default NULL,
-  `fechacreacion` date default NULL,
-  `modificador` varchar(255) collate ".$cotejamiento." default NULL,
-  `fechamodificacion` date default NULL,
-  `keywordscontenido` varchar(255) collate ".$cotejamiento." default NULL,
-  `descripcioncontenido` text collate ".$cotejamiento.",
-  `fijo` int(11) default '0',
-  `activo` int(11) default '1',
-   PRIMARY KEY (`id`)) 
-  ENGINE=MyISAM DEFAULT CHARSET=".$charset." COLLATE=".$cotejamiento." ");
+$qryCreateTable = "CREATE TABLE vadmon_contenidos (";
+$qryCreateTable .="id SERIAL PRIMARY KEY, subcontenido VARCHAR(255), menucontenido VARCHAR(255) DEFAULT NULL, ordencontenido INTEGER DEFAULT 0,";
+$qryCreateTable .="titulocontenido VARCHAR(255) DEFAULT NULL,textocontenido TEXT, imagen1contenido VARCHAR(255) DEFAULT NULL,";
+$qryCreateTable .="imagen2contenido VARCHAR(255) DEFAULT NULL, imagen3contenido VARCHAR(255) DEFAULT NULL, creador VARCHAR(255) DEFAULT NULL,";
+$qryCreateTable .="fechacreacion DATE DEFAULT NULL, modificador VARCHAR(255) DEFAULT NULL, fechamodificacion DATE DEFAULT NULL,";
+$qryCreateTable .="keywordscontenido VARCHAR(255) DEFAULT NULL, descripcioncontenido TEXT, fijo INTEGER DEFAULT 0, activo INTEGER DEFAULT 1);";
+$result = pg_query($conexion, $qryCreateTable);
 ?>
-<script languaje='javascript' type='text/javascript'>//window.close();</script>
-<?php echo $result ?>
+<script languaje='javascript' type='text/javascript'>window.close();</script>
