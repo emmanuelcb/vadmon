30318b58380ef5aaa1df70254471a12486811221
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-27 15:19:07 -0500

    Updated conexion.php

--- conexion.php
+++ conexion.php
@@ -27,35 +27,33 @@ if(isset($_SESSION["idUsuarioVAdmon"])){
 	$claseVAdmon->sesionSeguraVAdmonInicio();
 	$idSesion = $_SESSION["idUsuarioVAdmon"];
 }
-if($stmtPlan = $planconexion->prepare("SELECT id, usuario, planusuarios, planpermisos, planarticulos, plannoticias, planencuestas, planpromociones, plandisenio, planregistro, servidor, servidorbd, servidorusuario, servidorpass, servidorftpdirectorio, servidorftpusuario, servidorftppass FROM vadmon_planes WHERE id = $idSesion LIMIT 1")) {
-	$stmtPlan->execute();
-	$stmtPlan->store_result();
-	$stmtPlan->bind_result($id_rslt, $usuario_rslt, $planusuarios_rslt, $planpermisos_rslt, $planarticulos_rslt, $plannoticias_rslt, $planencuestas_rslt, $planpromociones_rslt, $plandisenio_rslt,  $planregistro_rslt, $servidor_rslt, $servidorbd_rslt, $servidorusuario_rslt, $servidorpass_rslt, $servidorftpdirectorio_rslt, $servidorftpusuario_rslt, $servidorftppass_rslt); 
-	if($stmtPlan->num_rows > 0) {
-		while ($stmtPlan->fetch()) {
-			$Id_p = $id_rslt;
-			$Usuario_p = $usuario_rslt;
-			$PlanUsuarios_p = $planusuarios_rslt;
-			$PlanPermisos_p = $planpermisos_rslt;
-			$PlanArticulos_p = $planarticulos_rslt;
-			$PlanNoticias_p = $plannoticias_rslt;
-			$PlanEncuestas_p = $planencuestas_rslt;
-			$PlanPromociones_p = $planpromociones_rslt;
-			$PlanDisenio_p = $plandisenio_rslt;
-			$PlanRegistro_p = $planregistro_rslt;
-			$HostDominio = $servidor_rslt;
-			$DBDominio = $servidorbd_rslt;
-			$UserDominio = $servidorusuario_rslt;
-			$PassDominio = $servidorpass_rslt;
-			$FTPDominioDir = $servidorftpdirectorio_rslt;
-			$FTPUsuario = $servidorftpusuario_rslt;
-			$FTPPass = $servidorftppass_rslt;
+$strGetPlanSQL = 'SELECT id, usuario, planusuarios, planpermisos, planarticulos, plannoticias, planencuestas, planpromociones, plandisenio, planregistro, servidor, servidorbd, servidorusuario, servidorpass, servidorftpdirectorio, servidorftpusuario, servidorftppass FROM vadmon_planes WHERE id = $1 LIMIT 1';
+$strGetPlanSQLName = 'getPlan';
+if(pg_prepare($planconexion, $strGetPlanSQLName, $strGetPlanSQL)) {
+  	$result = pg_execute($planconexion, $strGetPlanSQLName, array($idSesion));
+  	$fetchArr = pg_fetch_all($result);
+	if(sizeof(fetchArr) > 0) {
+      	while($row = pg_fetch_array($result) {
+			$Id_p = $row['id'];
+			$Usuario_p = $row['usuario'];
+			$PlanUsuarios_p = $row['planusuarios'];
+			$PlanPermisos_p = $row['planpermisos'];
+			$PlanArticulos_p = $row['planarticulos'];
+			$PlanNoticias_p = $row['plannoticias'];
+			$PlanEncuestas_p = $row['planencuestas'];
+			$PlanPromociones_p = $row['planpromociones'];
+			$PlanDisenio_p = $row['plandisenio'];
+			$PlanRegistro_p = $row['planregistro'];
+			$HostDominio = $row['servidor'];
+			$DBDominio = $row['servidordb'];
+			$UserDominio = $row['servidorusuario'];
+			$PassDominio = $row['servidorpass'];
+			$FTPDominioDir = $row['servidorftpdirectorio'];
+			$FTPUsuario = $row['servidorftpusuario'];
+			$FTPPass = $row['servidorftppass'];
 		}
 	}
 }
-$conexion = new mysqli ($HostDominio, $UserDominio, $PassDominio, $DBDominio);
-/*if ($conexion->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
-}
-echo $conexion->host_info . "\n";*/
+$strPgConnection = 'dbname='.$DBDominio.' host='.$HostDominio.' port=5432 user='.$UserDominio.' password='.$PassDominio.' sslmode=require';
+$conexion = pg_connect($strPgConnection);
 ?>
