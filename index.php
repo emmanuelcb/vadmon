839c9b0706dbce770229c2a2f7fd2c32a770a683
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-27 16:33:20 -0500

    Updated index.php

--- index.php
+++ index.php
@@ -6,13 +6,11 @@ include("inicioSesionVAdmonClass.php");
 $claseVAdmon = new inicioSesionVAdmonClass;
 $claseVAdmon->sesionSeguraVAdmonInicio();
 if($claseVAdmon->revisaInicioVAdmon($planconexion) == true){
-  	echo 'It\'s true';
 	include("ivariables.php");
 	include('encabezado.php');
 	include('contenedor.php');
 	include('pie.php');
 }else{
-  	echo 'It\'s false';
 	include("planvariables.php");
 	include("planencabezado.php");
 	include("plancontenedor.php");
