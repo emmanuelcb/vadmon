81c18652dc988f7b8d147673af1b15ba0aa7c106
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-27 09:37:31 -0500

    Updated planconexion.php

--- planconexion.php
+++ planconexion.php
@@ -1,7 +1,10 @@
 <?php
-$HostVAdmon = 'localhost';
-$UserVAdmon = 'root';
-$PassVAdmon = 'root';
-$DBVAdmon = 'vemeweb';
-$planconexion = new mysqli ($HostVAdmon, $UserVAdmon, $PassVAdmon, $DBVAdmon);
+$HostVAdmon = 'ec2-54-83-5-43.compute-1.amazonaws.com';
+$UserVAdmon = 'yrlszffmgftqyo';
+$PassVAdmon = '3PbYU_D4huXaZkwcxg8T_CmjOA';
+$DBVAdmon = 'dau3o208fvurb8';
+
+$strPgConnection = 'dbname='.$DBVAdmon.' host='.$HostVAdmon.' port=5432 ';	
+$strPgConnection .= 'user='.$UserVAdmon.' password='.$PassVAdmon.' sslmode=require';
+$planconexion = pg_connect($strPgConnection);
 ?>
