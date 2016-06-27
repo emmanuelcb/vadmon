109f8913bec931cf658a2d4ca6f01f026b963b6d
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-27 16:34:50 -0500

    Updated planvariables.php

--- planvariables.php
+++ planvariables.php
@@ -1,17 +1,18 @@
 <?php
 // Revisa si la tabla de planes esta instalada
+echo 'instalando planes';
 $sqlPlanesTable = "SHOW TABLES LIKE vadmon_planes";
 $sqlPlanesName = "isThereExistingPlanesTable";
 if(pg_prepare($planconexion, $sqlPlanesName, $sqlPlanesTable))
 {
   $result = pg_execute($planconexion, $sqlPlanesName);
-  echo $result;
   $fetchArr = pg_fetch_all($result);
   if(sizeof($fetchArr) == 0) {
     include("acciones/instalacion/vadmon_planes.php");
   }
 }
 //Revisa si la tabla de inicios esta instalada
+echo 'instalando planes inicios';
 $sqlPlanesIniciosTable = "SHOW TABLES LIKE vadmon_planesinicios";
 $sqlPlanesIniciosName = "isThereExistingPlanesIniciosTable";
 if(pg_prepare($planconexion, $sqlPlanesIniciosName, $sqlPlanesIniciosTable))
