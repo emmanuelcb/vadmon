999a867155ae81aaaa6fbb81974c23ff84b0700f
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-27 16:49:11 -0500

    Updated planvariables.php

--- planvariables.php
+++ planvariables.php
@@ -3,10 +3,10 @@
 $sqlPlanesTable = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'vadmon_planes'";
 $sqlPlanesName = "isThereExistingPlanesTable";
 echo pg_prepare($planconexion, $sqlPlanesName, $sqlPlanesTable);
+echo pg_execute($planconexion, $sqlPlanesName);
 if(pg_prepare($planconexion, $sqlPlanesName, $sqlPlanesTable))
 {
   $result = pg_execute($planconexion, $sqlPlanesName);
-  echo $result;
   $fetchArr = pg_fetch_all($result);
   if(sizeof($fetchArr) == 0) {
     include("acciones/instalacion/vadmon_planes.php");
@@ -16,6 +16,7 @@ if(pg_prepare($planconexion, $sqlPlanesName, $sqlPlanesTable))
 $sqlPlanesIniciosTable = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'vadmon_planesinicios'";
 $sqlPlanesIniciosName = "isThereExistingPlanesIniciosTable";
 echo pg_prepare($planconexion, $sqlPlanesIniciosName, $sqlPlanesIniciosTable);
+echo pg_execute($planconexion, $sqlPlanesIniciosName);
 if(pg_prepare($planconexion, $sqlPlanesIniciosName, $sqlPlanesIniciosTable))
 {
   $result = pg_execute($planconexion, $sqlPlanesName);
