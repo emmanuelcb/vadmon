46da6182f3187f3486ea78c6a99b82831859feb4
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-27 08:38:46 -0500

    Updated generaCookie.php

--- instalacion/generaCookie.php
+++ instalacion/generaCookie.php
@@ -1,9 +1,9 @@
 <?php
-setcookie("servidor", "".$_POST["servidor"]."", time()+(3600 * 24));
-setcookie("baseDatos", "".$_POST["baseDatos"]."", time()+(3600 * 24));
-setcookie("usuarioBase", "".$_POST["usuarioBase"]."", time()+(3600 * 24));
+setcookie("servidor", "".$_POST["servidor"]."", time()+(3600 * 24)); // host
+setcookie("baseDatos", "".$_POST["baseDatos"]."", time()+(3600 * 24)); // dbName
+setcookie("usuarioBase", "".$_POST["usuarioBase"]."", time()+(3600 * 24)); // user
 if($_POST["passBase"] <>''){
-	setcookie("passBase", "".$_POST["passBase"]."", time()+(3600 * 24));
+	setcookie("passBase", "".$_POST["passBase"]."", time()+(3600 * 24)); // pass
 }else{
 	setcookie("passBase", "ninguna", time()+(3600 * 24));
 }
