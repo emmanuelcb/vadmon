3f2ac3fa72163cfc0419d0143579bc389d14ff11
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-27 17:07:22 -0500

    Updated vadmon_planes.php

--- acciones/instalacion/vadmon_planes.php
+++ acciones/instalacion/vadmon_planes.php
@@ -5,7 +5,7 @@ $strCreatePlanesTableSQL .= 'id SERIAL PRIMARY KEY, usuario VARCHAR(30) NOT NULL
 $strCreatePlanesTableSQL .= 'planarticulos BOOLEAN DEFAULT FALSE, plannoticias BOOLEAN DEFAULT FALSE, planencuestas BOOLEAN FALSE, planpromociones BOOLEAN DEFAULT FALSE,';
 $strCreatePlanesTableSQL .= 'plandisenio BOOLEAN DEFAULT FALSE, planregistro BOOLEAN DEFAULT FALSE, servidor VARCHAR(255) NOT NULL, servidordb VARCHAR(255) NOT NULL,';
 $strCreatePlanesTableSQL .= 'servidorusuario VARCHAR(255) NOT NULL, servidorpass VARCHAR(255) NOT NULL, servidorftpdirectorio VARCHAR(255) NOT NULL,';
-$strCreatePlanesTableSQL .= 'servidorftpusuario VARCHAR(255) NOT NULL, servidorftppass VARCHAR(255) NOT NULL, activo BOOLEAN DEFAULT true';
+$strCreatePlanesTableSQL .= 'servidorftpusuario VARCHAR(255) NOT NULL, servidorftppass VARCHAR(255) NOT NULL, activo BOOLEAN DEFAULT TRUE';
 $strCreatePlanesTableSQL .= ');';
 $strCreatePlanesTableSQLName = 'createPlanes';
 if(pg_prepare($planconexion, $strCreatePlanesTableSQLName, $strCreatePlanesTableSQL))
