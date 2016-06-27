85ab0e97e554dd72ea7e1ff9250dc4809420975d
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-27 17:32:10 -0500

    Updated vadmon_planesinicios.php

--- acciones/instalacion/vadmon_planesinicios.php
+++ acciones/instalacion/vadmon_planesinicios.php
@@ -3,9 +3,5 @@
 $strCreatePlanesIniciosTable = 'CREATE TABLE vadmon_planesinicios (';
 $strCreatePlanesIniciosTable .= 'id SERIAL PRIMARY KEY, idusuario VARCHAR(30) NOT NULL, tiempo TIME';
 $strCreatePlanesIniciosTable .= ');';
-$strCreatePlanesIniciosTableName = 'createPlanesIniciosTable';
-if(pg_prepare($planconexion, $strCreatePlanesIniciosTableName, $strCreatePlanesIniciosTable))
-{
-	$result = pg_execute($planconexion, $strCreatePlanesIniciosTableName);
-}
+pg_query($planconexion, $strCreatePlanesIniciosTable);
 ?>
