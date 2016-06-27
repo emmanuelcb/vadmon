9bf8849ed87daed227f7a53803e4c4fd768ab22e
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-27 16:50:58 -0500

    Updated planvariables.php

--- planvariables.php
+++ planvariables.php
@@ -2,10 +2,9 @@
 // Revisa si la tabla de planes esta instalada
 $sqlPlanesTable = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'vadmon_planes'";
 $sqlPlanesName = "isThereExistingPlanesTable";
-echo pg_prepare($planconexion, $sqlPlanesName, $sqlPlanesTable);
-echo pg_execute($planconexion, $sqlPlanesName);
 if(pg_prepare($planconexion, $sqlPlanesName, $sqlPlanesTable))
 {
+  echo 'I am in';
   $result = pg_execute($planconexion, $sqlPlanesName);
   $fetchArr = pg_fetch_all($result);
   if(sizeof($fetchArr) == 0) {
@@ -15,8 +14,6 @@ if(pg_prepare($planconexion, $sqlPlanesName, $sqlPlanesTable))
 //Revisa si la tabla de inicios esta instalada
 $sqlPlanesIniciosTable = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'vadmon_planesinicios'";
 $sqlPlanesIniciosName = "isThereExistingPlanesIniciosTable";
-echo pg_prepare($planconexion, $sqlPlanesIniciosName, $sqlPlanesIniciosTable);
-echo pg_execute($planconexion, $sqlPlanesIniciosName);
 if(pg_prepare($planconexion, $sqlPlanesIniciosName, $sqlPlanesIniciosTable))
 {
   $result = pg_execute($planconexion, $sqlPlanesName);
