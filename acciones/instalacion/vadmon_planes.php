ca8bc93dd8c9773693582f29f451e191fe4cb995
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-27 15:52:18 -0500

    Updated vadmon_planes.php

--- acciones/instalacion/vadmon_planes.php
+++ acciones/instalacion/vadmon_planes.php
@@ -1,26 +1,14 @@
 <?php
 // Creamos la tabla
-if($stmtCrearVadmonPlanes = $planconexion->prepare( "CREATE TABLE `vadmon_planes` (
-                                                    `id` smallint(5) NOT NULL auto_increment,
-                                                    `usuario` varchar(30) NOT NULL,
-                                                    `contrasenia` varchar(32) NOT NULL,
-                                                    `planusuarios` INT(1) NOT NULL DEFAULT '0',
-                                                    `planpermisos` INT(1) NOT NULL DEFAULT '0',
-                                                    `planarticulos` INT(1) NOT NULL DEFAULT '0',
-                                                    `plannoticias` INT(1) NOT NULL DEFAULT '0',
-                                                    `planencuestas` INT(1) NOT NULL DEFAULT '0',
-                                                    `planpromociones` INT(1) NOT NULL DEFAULT '0',
-                                                    `plandisenio` INT(1) NOT NULL DEFAULT '0',
-                                                    `planregistro` INT(1) NOT NULL DEFAULT '0',
-                                                    `servidor` VARCHAR(255) NOT NULL,
-                                                    `servidorbd` VARCHAR(255) NOT NULL,
-                                                    `servidorusuario` VARCHAR(255) NOT NULL,
-                                                    `servidorpass` VARCHAR(255) NOT NULL,
-                                                    `servidorftpdirectorio` VARCHAR(255) NOT NULL,
-                                                    `servidorftpusuario` VARCHAR(255) NOT NULL,
-                                                    `servidorftppass` VARCHAR(255) NOT NULL,
-                                                    `activo` int(11) default '1',
-                                                    PRIMARY KEY (`id`)) ENGINE=MyISAM" ))
+$strCreatePlanesTableSQL = 'CREATE TABLE vadmon_planes (';
+$strCreatePlanesTableSQL .= 'id SERIAL PRIMARY KEY, usuario VARCHAR(30) NOT NULL, contrasenia VARCHAR(32) NOT NULL, planusuarios BOOLEAN DEFAULT false, planpermisos BOOLEAN DEFAULT false,';
+$strCreatePlanesTableSQL .= 'planarticulos BOOLEAN DEFAULT false, plannoticias BOOLEAN DEFAULT false, planencuestas BOOLEAN false, planpromociones BOOLEAN DEFAULT false,';
+$strCreatePlanesTableSQL .= 'plandisenio BOOLEAN DEFAULT false, planregistro BOOLEAN DEFAULT false, servidor VARCHAR(255) NOT NULL, servidordb VARCHAR(255) NOT NULL,';
+$strCreatePlanesTableSQL .= 'servidorusuario VARCHAR(255) NOT NULL, servidorpass VARCHAR(255) NOT NULL, servidorftpdirectorio VARCHAR(255) NOT NULL,';
+$strCreatePlanesTableSQL .= 'servidorftpusuario VARCHAR(255) NOT NULL, servidorftppass VARCHAR(255) NOT NULL, activo BOOLEAN DEFAULT true';
+$strCreatePlanesTableSQL .= ');';
+$strCreatePlanesTableSQLName = 'createPlanes';
+if(pg_prepare($planconexion, $strCreatePlanesTableSQLName, $strCreatePlanesTableSQL))
 {
 	$stmtCrearVadmonPlanes->execute();
 }
