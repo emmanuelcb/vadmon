8faf4e5158c6f7d0a194623c61caf84538382cbe
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-27 13:40:43 -0500

    Updated instalaContenidos.php

--- instalacion/instalaContenidos.php
+++ instalacion/instalaContenidos.php
@@ -14,7 +14,7 @@ $qryCreateTable = "CREATE TABLE vadmon_contenidos (";
 $qryCreateTable .="id SERIAL PRIMARY KEY, subcontenido VARCHAR(255), menucontenido VARCHAR(255) DEFAULT NULL, ordencontenido INTEGER DEFAULT 0,";
 $qryCreateTable .="titulocontenido VARCHAR(255) DEFAULT NULL,textocontenido TEXT, imagen1contenido VARCHAR(255) DEFAULT NULL,";
 $qryCreateTable .="imagen2contenido VARCHAR(255) DEFAULT NULL, imagen3contenido VARCHAR(255) DEFAULT NULL, creador VARCHAR(255) DEFAULT NULL,";
-$qryCreateTable .="fechacreacion DATE DEFAULT NOW(), modificador VARCHAR(255) DEFAULT NULL, fechamodificacion DATE DEFAULT NOW(),";
+$qryCreateTable .="fechacreacion DATE DEFAULT NULL, modificador VARCHAR(255) DEFAULT NULL, fechamodificacion DATE DEFAULT NULL,";
 $qryCreateTable .="keywordscontenido VARCHAR(255) DEFAULT NULL, descripcioncontenido TEXT, fijo INTEGER DEFAULT 0, activo INTEGER DEFAULT 1);";
 $result = pg_query($conexion, $qryCreateTable);
 ?>
