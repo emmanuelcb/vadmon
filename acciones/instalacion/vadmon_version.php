091fe2302c6e4e8f385dfea920c8514910c84f71
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-28 09:07:20 -0500

    Updated vadmon_version.php

--- acciones/instalacion/vadmon_version.php
+++ acciones/instalacion/vadmon_version.php
@@ -1,10 +1,7 @@
 <?php
 // Creamos la tabla
 $strCreateVersionTableSQL = 'CREATE TABLE vadmon_version(id SERIAL PRIMARY KEY, version VARCHAR(255) NOT NULL)';
-$strCreateVersionTableSQLName = 'createVersionTable';
-if(pg_prepare($conexion, $strCreateVersionTableSQLName, $strCreateVersionTableSQL)) {
-	$result = pg_execute($conexion, $strCreateVersionTableSQLName);
-}
+$result = pg_execute($conexion, $strCreateVersionTableSQL);
 // Colocamos la version actual
 $strInsertVersionSQL = 'INSERT INTO vadmon_version (version) VALUES ($1)';
 $strInsertVersionSQLName = 'insertVersion';
