8f25ffb50565e85e1315c7e35b81ac9baa7493ab
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-28 10:11:11 -0500

    Updated vadmon_configuracion.php

--- acciones/instalacion/vadmon_configuracion.php
+++ acciones/instalacion/vadmon_configuracion.php
@@ -1,17 +1,16 @@
 <?php
 $strCreateConfiguracionTableSQL = 'CREATE TABLE vadmon_configuracion (';
 $strCreateConfiguracionTableSQL .= 'id SERIAL PRIMARY KEY, herramienta VARCHAR(255) NOT NULL, campo1 VARCHAR(255) NOT NULL, campo2 VARCHAR(255) NOT NULL,';
-$strCreateConfiguracionTableSQL .= 'creador VARCHAR(255) DEFAULT NULL, fechacreacion DATE DEFAULT CURRENT_DATE, modificador VARCHAR(255) DEFAULT NULL, fechamodificacion DATE DEFAULT CURRENT_DATE, fijo BOOLEAN DEFAULT FALSE, activo BOOLEAN DEFAULT TRUE);';
-$strCreateConfiguracionTableSQL .= '';
-$strCreateConfiguracionTableSQL .= '';
-$strCreateConfiguracionTableSQL .= '';
-$strCreateConfiguracionTableSQL .= '';
-$strCreateConfiguracionTableSQL .= '';
+$strCreateConfiguracionTableSQL .= 'creador VARCHAR(255) DEFAULT NULL, fechacreacion DATE DEFAULT CURRENT_DATE, modificador VARCHAR(255) DEFAULT NULL,';
+$strCreateConfiguracionTableSQL .= 'fechamodificacion DATE DEFAULT CURRENT_DATE, fijo BOOLEAN DEFAULT FALSE, activo BOOLEAN DEFAULT TRUE);';
 pg_query($conexion, $strCreateConfiguracionTableSQL);
 
 // Creamos limite de contenidos principales
-mysql_query("insert into vadmon_configuracion 
-(id, herramienta, campo1, campo2) values 
-('1','limitecontenidos', '20', '1')")
-or die("Could not insert data because ".mysql_error());
+$strInsertContenidosLimitsSQL = 'INSERT INTO vadmon_configuration (id, herramienta, campo1, campo2)';
+$strInsertContenidosLimitsSQL .= 'VALUES (1, \'limitecontenidos\', 20, TRUE)';
+$strInsertContenidosLimitsSQLName = 'insertContenidosLimits';
+if(pg_prepare($conexion, $strInsertContenidosLimitsSQLName, $strInsertContenidosLimitsSQL))
+{
+	pg_execute($conexion, $strInsertContenidosLimitsSQLName);
+}
 ?>
