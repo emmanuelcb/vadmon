da1cb9563eedd5b26f6312ecc406e1ba09cd0217
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-28 09:26:16 -0500

    Updated vadmon_permisos.php

--- acciones/instalacion/vadmon_permisos.php
+++ acciones/instalacion/vadmon_permisos.php
@@ -1,28 +1,20 @@
 <?php
-mysql_query("CREATE TABLE `vadmon_permisos` (
-  `id` int(11) NOT NULL auto_increment,
-  `nivelusuario` varchar(255) collate latin1_spanish_ci default NULL,
-  `contenidos` int(11) default '0',
-  `noticias` int(11) default '0',
-  `articulos` int(11) default '0',
-  `promociones` int(11) default '0',
-  `banners` int(11) default '0',
-  `usuarios` int(11) default '0',
-  `configuracion` int(11) default '0',
-  `diseno` int(11) default '0',
-  `encuestas` int(11) default '0',
-  `basesdedatos` int(11) default '0',
-  `permisos` int(11) default '0',
-  `papelera` int(11) default '0',
-  `editar` int(11) default '0',
-  `crear` int(11) default '0',
-  `eliminar` int(11) default '0',
-   PRIMARY KEY (`id`)) 
-  ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci ") ;
+// Creamos la tabla
+$strCreatePermisosTableSQL = 'CREATE TABLE vadmon_permisos (';
+$strCreatePermisosTableSQL .= 'id SERIAL PRIMARY KEY, nivelusuario VARCHAR(255) DEFAULT NULL, contenidos BOOLEAN DEFAULT FALSE, noticias BOOLEAN DEFAULT FALSE,';
+$strCreatePermisosTableSQL .= 'articulos BOOLEAN DEFAULT FALSE, promociones BOOLEAN DEFAULT FALSE, banners BOOLEAN DEFAULT FALSE, usuarios BOOLEAN DEFAULT FALSE,';
+$strCreatePermisosTableSQL .= 'configuracion BOOLEAN DEFAULT FALSE, diseno BOOLEAN DEFAULT FALSE, encuestas BOOLEAN DEFAULT FALSE, basededatos BOOLEAN DEFAULT FALSE,';
+$strCreatePermisosTableSQL .= 'permisos BOOLEAN DEFAULT FALSE, papelera BOOLEAN DEFAULT FALSE, editar BOOLEAN DEFAULT FALSE, crear BOOLEAN DEFAULT FALSE, eliminar BOOLEAN DEFAULT FALSE);';
+$result = pg_execute($conexion, $strCreatePermisosTableSQL);
   
 // Creamos limite de contenidos principales
-mysql_query("insert into vadmon_permisos
-(id, nivelusuario, contenidos, noticias, articulos, promociones, banners, usuarios, configuracion, diseno, encuestas, basesdedatos, permisos, papelera, editar, crear, eliminar) values 
-('1', 'maestro', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1','1')")
-or die("Could not insert data because ".mysql_error());
+$strInsertBasicPermisosSQL = 'INSERT INTO vadmon_permisos (';
+$strInsertBasicPermisosSQL .= 'id, nivelusuario, contenidos, noticias, articulos, promociones, banners, usuarios, configuracion, diseno, encuestas, basesdedatos, permisos, papelera, editar, crear, eliminar';
+$strInsertBasicPermisosSQL .= 'VALUES (';
+$strInsertBasicPermisosSQL .= '1, \'maestro\', TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE ,TRUE)';
+$strInsertBasicPermisosSQLName = 'insertBasicPermisos';
+if(pg_prepare($conexion, $strInsertBasicPermisosSQLName, $strInsertBasicPermisosSQL))
+{
+	pg_execute($conexion, $strInsertBasicPermisosSQLName);
+}
 ?>
