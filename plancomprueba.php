ed1e92cb5002672fe46671f615e1791d21c9bdb6
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-29 12:15:49 -0500

    Updated plancomprueba.php

--- plancomprueba.php
+++ plancomprueba.php
@@ -7,9 +7,11 @@ if(isset($_POST['usuario'], $_POST['contrasenia'])) {
    $usuario = $_POST['usuario'];
    $contrasenia = $_POST['contrasenia'];
    if($claseVAdmon->inicioSesionVAdmon($usuario, $contrasenia, $planconexion) == true) {
-        header("location: index.php?mensaje=correctoInicio&nombreUsuario=".$_POST["usuario"]);
+        echo TRUE;
+     	//header("location: index.php?mensaje=correctoInicio&nombreUsuario=".$_POST["usuario"]);
    } else {
-        header("location: index.php?mensaje=errorUsuario&nombreUsuario=".$_POST['usuario']);
+        echo FALSE;
+     	//header("location: index.php?mensaje=errorUsuario&nombreUsuario=".$_POST['usuario']);
    }
 } else {
 	header("location: index.php?mensaje=errorUsuario&nombreUsuario=".$_POST['usuario']);
