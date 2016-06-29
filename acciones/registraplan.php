e396c66c406ead5680aaf22494f2985d79540dba
emmanuelcb <emmanuel.cb@live.com.mx>
2016-06-29 09:32:53 -0500

    Updated registraplan.php

--- acciones/registraplan.php
+++ acciones/registraplan.php
@@ -4,9 +4,8 @@ include("../planconexion.php");
 $strInsertUsuarioPlanSQLName = 'InsertUsuarioPlan';
 $strInsertUsuarioPlanSQL = 'INSERT INTO vadmon_planes (id, usuario, contrasenia, planusuarios, planpermisos, planarticulos, plannoticias, planencuestas, planpromociones, plandisenio, planregistro, servidor, servidordb, servidorusuario, servidorpass, servidorftpdirectorio, servidorftpusuario, servidorftppass, activo)';
 $strInsertUsuarioPlanSQL .= 'VALUES(1, $1, $2, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, \'ec2-54-83-5-43.compute-1.amazonaws.com\', \'dau3o208fvurb8\', \'yrlszffmgftqyo\', \'3PbYU_D4huXaZkwcxg8T_CmjOA\', \'test\', \'test\', \'test\', TRUE)';
-echo $strInsertUsuarioPlanSQL;
-/*if(pg_prepare($planconexion, $strInsertUsuarioPlanSQLName, $strInsertUsuarioPlanSQL)
+if(pg_prepare($planconexion, $strInsertUsuarioPlanSQLName, $strInsertUsuarioPlanSQL))
 {
   	pg_execute($planconexion, $strInsertUsuarioPlanSQLName, array($_POST["usuario"], $_POST["contrasenia"]));
-}*/
+}
 ?>
