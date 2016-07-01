afa3ddf8b48e50a0a51fcadc9a52d43280878f5d
emmanuelcb <emmanuel.cb@live.com.mx>
2016-07-01 13:14:51 -0500

    Updated instalaContenidos.php

--- instalacion/instalaContenidos.php
+++ instalacion/instalaContenidos.php
@@ -1,21 +1,9 @@
 <?php
-$servidor = $_COOKIE['servidor'];
-$baseDatos = $_COOKIE['baseDatos'];
-$usuarioBase = $_COOKIE['usuarioBase'];
-if($_COOKIE['passBase'] <> 'ninguna'){ $passBase = $_COOKIE['passBase']; }else{ $passBase = ''; }
-$charset = $_COOKIE['charset'];
-$cotejamiento = $_COOKIE['cotejamiento'];
-
-$strPgConnection = 'dbname='.$baseDatos.' host='.$servidor.' port=5432 ';	
-$strPgConnection .= 'user='.$usuarioBase.' password='.$passBase.' sslmode=require';
-$conexion = pg_connect($strPgConnection);
-
-$qryCreateTable = "CREATE TABLE vadmon_contenidos (";
-$qryCreateTable .="id SERIAL PRIMARY KEY, subcontenido VARCHAR(255), menucontenido VARCHAR(255) DEFAULT NULL, ordencontenido INTEGER DEFAULT 0,";
-$qryCreateTable .="titulocontenido VARCHAR(255) DEFAULT NULL,textocontenido TEXT, imagen1contenido VARCHAR(255) DEFAULT NULL,";
-$qryCreateTable .="imagen2contenido VARCHAR(255) DEFAULT NULL, imagen3contenido VARCHAR(255) DEFAULT NULL, creador VARCHAR(255) DEFAULT NULL,";
-$qryCreateTable .="fechacreacion DATE DEFAULT CURRENT_DATE, modificador VARCHAR(255) DEFAULT NULL, fechamodificacion DATE DEFAULT CURRENT_DATE,";
-$qryCreateTable .="keywordscontenido VARCHAR(255) DEFAULT NULL, descripcioncontenido TEXT, fijo INTEGER DEFAULT 0, activo INTEGER DEFAULT 1);";
-$result = pg_query($conexion, $qryCreateTable);
+$qryCreateContenidos = "CREATE TABLE vadmon_contenidos (";
+$qryCreateContenidos .="id SERIAL PRIMARY KEY, subcontenido VARCHAR(255), menucontenido VARCHAR(255) DEFAULT NULL, ordencontenido INTEGER DEFAULT 0,";
+$qryCreateContenidos .="titulocontenido VARCHAR(255) DEFAULT NULL,textocontenido TEXT, imagen1contenido VARCHAR(255) DEFAULT NULL,";
+$qryCreateContenidos .="imagen2contenido VARCHAR(255) DEFAULT NULL, imagen3contenido VARCHAR(255) DEFAULT NULL, creador VARCHAR(255) DEFAULT NULL,";
+$qryCreateContenidos .="fechacreacion DATE DEFAULT CURRENT_DATE, modificador VARCHAR(255) DEFAULT NULL, fechamodificacion DATE DEFAULT CURRENT_DATE,";
+$qryCreateContenidos .="keywordscontenido VARCHAR(255) DEFAULT NULL, descripcioncontenido TEXT, fijo INTEGER DEFAULT 0, activo INTEGER DEFAULT 1);";
+$result = pg_query($conexion, $qryCreateContenidos);
 ?>
-<script languaje='javascript' type='text/javascript'>window.close();</script>
