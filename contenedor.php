cb9cde490f83a996137f8edfcdf26a40aa603178
emmanuelcb <emmanuel.cb@live.com.mx>
2016-07-05 16:53:51 -0500

    Updated contenedor.php

--- contenedor.php
+++ contenedor.php
@@ -1,7 +1,7 @@
 <table cellpadding="0" cellspacing="0" border="0" width="100%" class="fdpagina" id="fdpagina">
   <tr>
     <td class="barraLateral">
-	  <?php /*include('ibarraLateral.php');*/ ?>
+	  <?php include('ibarraLateral.php'); ?>
     </td>
     <td class="contenedor">
 	  <?php
