0fcdee81bc57f48a7ed9dafc07c976fad7d7b89c
emmanuelcb <emmanuel.cb@live.com.mx>
2016-07-05 12:18:42 -0500

    Updated comprueba.php

--- comprueba.php
+++ comprueba.php
@@ -5,17 +5,19 @@ $claseVAdmon = new inicioSesionVAdmonClass;
 $claseVAdmon->sesionSeguraVAdmonInicio();
 include ('conexion.php');
 
-if($stmt = $conexion->prepare("SELECT id, nivelusuario, nick, password FROM vadmon_usuarios where nick = ? AND password = ?")){
-	$stmt->bind_param("ss", $_POST["usuario"], $_POST["contrasenia"]);
-	$stmt->execute();
-	$stmt->store_result();
-	$stmt->bind_result($id_rslt, $nivelusuario_rslt, $nick_rslt, $password_rslt);
-	if($stmt->num_rows > 0) {
-		while($stmt->fetch()){
-			setcookie("idUsuario", "".$id_rslt."", time()+(3600 * 24));
-			setcookie("nivelUsuario", "".$nivelusuario_rslt."", time()+(3600 * 24));
-			setcookie("loggedin", "".$nick_rslt."", time()+(3600 * 24));
-			header("location: inicio.php?mensaje=correctoUsuario&nombreUsuario=".$nick_rslt);
+$strCompruebaSQLName = 'comprubaUsuario';
+$strCompruebaSQL = 'SELECT id, nivelusuario, nick, password FROM vadmon_usuarios where nick = $1 AND password = $2';
+if(pg_prepare($conexion, $strCompruebaSQLName, $strCompruebaSQL))
+{
+	$rslComprueba = pg_execute($conexion, $strCompruebaSQLName, array($_POST["usuario"], $_POST["contrasenia"]));
+  	$fetchComprueba = pg_fetch_all($rslComprueba);
+	if(sizeof($fetchComprueba) > 0) {
+		while($rowComprueba = pg_fetch_array($rslComprueba))
+        {
+			setcookie("idUsuario", "".$rowComprueba['id']."", time()+(3600 * 24));
+			setcookie("nivelUsuario", "".$rowComprueba['nivelusuario']."", time()+(3600 * 24));
+			setcookie("loggedin", "".$rowComprueba['nick']."", time()+(3600 * 24));
+			header("location: inicio.php?mensaje=correctoUsuario&nombreUsuario=".$rowComprueba['nick']);
 		}
 	}
 }
