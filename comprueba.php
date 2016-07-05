e12032b21040a3b2afdad21254c8e7d5a02ffed8
emmanuelcb <emmanuel.cb@live.com.mx>
2016-07-05 15:14:01 -0500

    Updated comprueba.php

--- comprueba.php
+++ comprueba.php
@@ -10,6 +10,8 @@ $strCompruebaSQL = 'SELECT id, nivelusuario, nick, password FROM vadmon_usuarios
 if(pg_prepare($conexion, $strCompruebaSQLName, $strCompruebaSQL))
 {
 	$rslComprueba = pg_execute($conexion, $strCompruebaSQLName, array($_POST["usuario"], $_POST["contrasenia"]));
+  	print_r($rslComprueba);
+  	echo '<br/>Size '.sizeof($rslComprueba);
 	if(sizeof($rslComprueba) > 0) {
 		while($rowComprueba = pg_fetch_assoc($rslComprueba))
         {
