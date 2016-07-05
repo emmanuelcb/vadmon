4328d99b7c28508a0956224ee50bcd955f741e13
emmanuelcb <emmanuel.cb@live.com.mx>
2016-07-05 15:17:08 -0500

    Updated comprueba.php

--- comprueba.php
+++ comprueba.php
@@ -12,8 +12,10 @@ if(pg_prepare($conexion, $strCompruebaSQLName, $strCompruebaSQL))
 	$rslComprueba = pg_execute($conexion, $strCompruebaSQLName, array($_POST["usuario"], $_POST["contrasenia"]));
   	$fetchComprueba = pg_fetch_all($rslComprueba);
 	if(sizeof($fetchComprueba) > 0) {
+      	echo 'there is an existing usuario';
 		while($rowComprueba = pg_fetch_assoc($rslComprueba))
         {
+          	print_r($rowComprueba);
 			setcookie("idUsuario", "".$rowComprueba['id']."", time()+(3600 * 24));
 			setcookie("nivelUsuario", "".$rowComprueba['nivelusuario']."", time()+(3600 * 24));
 			setcookie("loggedin", "".$rowComprueba['nick']."", time()+(3600 * 24));
