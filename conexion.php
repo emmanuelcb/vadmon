b398dd86f68c857fde71685daefaedf88aad2f36
emmanuelcb <emmanuel.cb@live.com.mx>
2016-07-05 13:54:09 -0500

    Updated conexion.php

--- conexion.php
+++ conexion.php
@@ -35,6 +35,7 @@ if($rslGetPlan = pg_query_params($planconexion, $strGetPlanSQL, array($idSesion)
   	print_r(pg_fetch_all($rslGetPlan));
 	if(pg_num_rows($rslGetPlan) > 0) {
       	while($row = pg_fetch_array($rslGetPlan)) {
+          	echo '<br/>';
           	print_r($row);
 			$Id_p = $row['id'];
 			$Usuario_p = $row['usuario'];
