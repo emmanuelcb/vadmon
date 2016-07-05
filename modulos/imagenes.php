670f410be8b98f0d0002a31b38fbd764cefe50d0
emmanuelcb <emmanuel.cb@live.com.mx>
2016-07-05 11:50:57 -0500

    Updated imagenes.php

--- modulos/imagenes.php
+++ modulos/imagenes.php
@@ -2,10 +2,15 @@
 /* //////////////////////
 	MOSTRAR IMAGENES
 ////////////////////// */
-if($stmtExisteVersion = $conexion->prepare("SHOW TABLES LIKE 'vadmon_imagenes'")) {
-	$stmtExisteVersion->execute();
-	$stmtExisteVersion->store_result();
-	if($stmtExisteVersion->num_rows == '') {
+$strImagenesTableExistsSQLName = 'isThereExistingImagenesTable';
+$strImagenesTableExistsSQL = 'SELECT table_name FROM information_schema.tables ';
+$strImagenesTableExistsSQL .= 'WHERE table_schema = \'vadmon_imagenes\'';
+if(pg_prepare($conexion, $strImagenesTableExistsSQLName, $strImagenesTableExistsSQL))
+{
+	$rslImagenesTableExists = pg_execute($conexion, $strImagenesTableExistsSQLName);
+	$fetchImagenesTableExists = pg_fetch_all($rslImagenesTableExists);
+	if(sizeof($fetchImagenesTableExists) == 0)
+    {
 		include("acciones/instalacion/vadmon_imagenes.php");
 	}
 }
@@ -24,10 +29,10 @@ if(isset($_GET["mostrarImagenes"]) && isset($_GET["carpeta"])){
 	// Si carpeta es igual a "galeria"
 	if($carpeta=="galeria"){ include("modulos/imagenes/galeria.php"); }
 }else if(isset($_GET["mostrarImagenes"])){
-	$mostrarContenido.='<div class="encabezadoSeccion"><span class="tituloSeccion">Imágenes</span><br/><br/>';
+	$mostrarContenido.='<div class="encabezadoSeccion"><span class="tituloSeccion">Im&aacute;genes</span><br/><br/>';
 	$mostrarContenido.='<span style="color:#999999;"><a href="inicio.php?mostrarImagenes=listar&carpeta=contenidos" title="Contenidos">Contenidos</a>';
 	$mostrarContenido.='&nbsp;&nbsp;|&nbsp;&nbsp;<a href="inicio.php?mostrarImagenes=listar&carpeta=banners" title="Banners">Banners</a>';
-	$mostrarContenido.='&nbsp;&nbsp;|&nbsp;&nbsp;<a href="inicio.php?mostrarImagenes=listar&carpeta=diseno" title="Diseño">Diseño</a>';
+	$mostrarContenido.='&nbsp;&nbsp;|&nbsp;&nbsp;<a href="inicio.php?mostrarImagenes=listar&carpeta=diseno" title="Diseno">Dise&ntilde;o</a>';
 	$mostrarContenido.='&nbsp;&nbsp;|&nbsp;&nbsp;<a href="inicio.php?mostrarImagenes=listar&carpeta=usuarios" title="Usuarios">Usuarios</a>';
 	$mostrarContenido.='&nbsp;&nbsp;|&nbsp;&nbsp;<a href="inicio.php?mostrarImagenes=listar&carpeta=promociones" title="Promociones">Promociones</a>';
 	$mostrarContenido.='</span></div>';
