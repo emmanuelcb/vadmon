727f29f54b1cbb302cdffe08eb56527d6d5bd22d
emmanuelcb <emmanuel.cb@live.com.mx>
2016-07-05 15:39:53 -0500

    Updated comprueba.php

--- comprueba.php
+++ comprueba.php
@@ -8,15 +8,13 @@ include ('conexion.php');
 $strCompruebaSQL = 'SELECT id, nivelusuario, nick, password FROM vadmon_usuarios where nick = $1';
 if($rslComprueba = pg_query_params($conexion, $strCompruebaSQL, array($_POST["usuario"])))
 {
-  	echo pg_num_rows($rslComprueba);
 	if(pg_num_rows($rslComprueba) > 0) {
 		while($rowComprueba = pg_fetch_assoc($rslComprueba))
         {
-			/*setcookie("idUsuario", "".$rowComprueba['id']."", time()+(3600 * 24));
+			setcookie("idUsuario", "".$rowComprueba['id']."", time()+(3600 * 24));
 			setcookie("nivelUsuario", "".$rowComprueba['nivelusuario']."", time()+(3600 * 24));
 			setcookie("loggedin", "".$rowComprueba['nick']."", time()+(3600 * 24));
-			header("location: inicio.php?mensaje=correctoUsuario&nombreUsuario=".$rowComprueba['nick']);*/
-          	print_r($rowComprueba);
+			header("location: inicio.php?mensaje=correctoUsuario&nombreUsuario=".$rowComprueba['nick']);
 		}
 	}
 }
