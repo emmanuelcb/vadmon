473a0dd044e869645d47e3f6fa499197e8421db0
emmanuelcb <emmanuel.cb@live.com.mx>
2016-07-05 11:49:52 -0500

    Updated vadmon_imagenes.php

--- acciones/instalacion/vadmon_imagenes.php
+++ acciones/instalacion/vadmon_imagenes.php
@@ -1,6 +1,6 @@
 <?php
 // Creamos la tabla
 $strCreateImagenesTable = 'CREATE TABLE vadmon_imagenes (';
-$strCreateImagenesTable .= 'id SERIAL PRIMARY KEY, nombreimg TEXT, image OID, filesize BIGINT );';
+$strCreateImagenesTable .= 'id SERIAL PRIMARY KEY, nombreimg TEXT, image OID, filesize BIGINT, carpeta VARCHAR(255));';
 pg_query($conexion, $strCreateImagenesTable);
 ?>
