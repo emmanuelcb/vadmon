3c4602845dade0905a5a414159b6d798ade787ed
emmanuelcb <emmanuel.cb@live.com.mx>
2016-07-06 12:31:49 -0500

    Updated funciones.php

--- funciones.php
+++ funciones.php
@@ -9,10 +9,10 @@ class claseFunciones{
       $jsLog .= 'var frag = document.createDocumentFragment();';
       $jsLog .= 'var div = document.createElement(\'div\');';
       $jsLog .= 'var txt = document.createTextNode(\''.$cadena.'\');';
-      $jsLog .= 'var HTMLCollection = document.getElementsByTagName(\'body\');';
-      $jsLog .= 'console.log(HTMLCollection); ';
-      $jsLog .= 'var body = Array.apply(0, HTMLCollection);';
+      $jsLog .= 'var body = document.getElementsByTagName(\'body\');';
       $jsLog .= 'console.log(body); ';
+      $jsLog .= 'console.log(body[0]);';
+      $jsLog .= 'console.log(typeof([].slice.call(body)));';
       $jsLog .= 'div.appendChild(txt);';
       $jsLog .= 'div.setAttribute(\'style\', \'display:block;position:fixed;top:0;left:0;z-index:99;background-color:rgba(0,0,0,0.5);width:100%;height:50px;color:rgba(255,255,255,0.5);text-align:center;\');';
       $jsLog .= 'frag.appendChild(div);';
