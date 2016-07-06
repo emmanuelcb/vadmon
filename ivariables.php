9ec04355f5afd7010b8850b5f8c63fac2dc45ed1
emmanuelcb <emmanuel.cb@live.com.mx>
2016-07-06 12:48:03 -0500

    Updated ivariables.php

--- ivariables.php
+++ ivariables.php
@@ -234,7 +234,7 @@ if($archivoActual <> "index.php")
     if($rslPermisosTable = pg_query($conexion, $strPermisosTableExistsSQL))
     {
       if(pg_num_rows($rslPermisosTable) == 0) {
-        $mostrarContenidos .= $claseFunciones->log('include vadmon_permisos.php');
+        $javascriptExtra .= $claseFunciones->log('include vadmon_permisos.php');
         include("acciones/instalacion/vadmon_permisos.php");
       }
     }
@@ -245,7 +245,7 @@ if($archivoActual <> "index.php")
     if($rslUsuariosTable = pg_query($conexion, $strUsuariosTableExistsSQL))
     {
       if(pg_num_rows($rslUsuariosTable) == 0){
-        $mostrarContenidos .= $claseFunciones->log('include vadmon_usuarios.php');
+        $javascriptExtra .= $claseFunciones->log('include vadmon_usuarios.php');
         include("acciones/instalacion/vadmon_usuarios.php");
         $needNewUser = true;
       }
@@ -257,7 +257,7 @@ if($archivoActual <> "index.php")
       $strInsertUsrSQL = 'INSERT INTO vadmon_usuarios (nick, password, nombre, apellidos, email, avatar, nivelusuario, activo) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
       if($rslInsertUsr = pg_query_params($conexion, $strInsertUsrSQL, array('admin','admin','Administrador','Cruz','emmanuel.cb@outlook.com','avatar.jpg','maestro', TRUE)))
       {
-          $mostrarContenidos .= $claseFunciones->log('User Inserted');
+          $javascriptExtra .= $claseFunciones->log('User Inserted');
       }
     }
 }
