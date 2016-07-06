68c7533225d59294d4882f7147b175ce68a7ac8d
emmanuelcb <emmanuel.cb@live.com.mx>
2016-07-06 12:10:39 -0500

    Updated funciones.php

--- funciones.php
+++ funciones.php
@@ -12,7 +12,7 @@ class claseFunciones{
       $jsLog .= 'div.appendChild(txt);';
       $jsLog .= 'div.setAttribute(\'style\', \'display:block;position:fixed;top:0;left:0;z-index:99;background-color:rgba(0,0,0,0.5);width:100%;height:50px;color:rgba(255,255,255,0.5);text-align:center;\');';
       $jsLog .= 'frag.appendChild(div);';
-      $jsLog .= 'document.getElementByTagName(\'body\')[0].appendChild(frag);';
+      $jsLog .= 'document.getElementsByTagName(\'body\')[0].appendChild(frag);';
       $jsLog .= '</script>';
       return $jsLog;
     }
