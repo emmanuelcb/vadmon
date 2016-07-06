157d938325d851406b693b4a849d103ed62625f8
emmanuelcb <emmanuel.cb@live.com.mx>
2016-07-06 09:38:46 -0500

    Updated funciones.php

--- funciones.php
+++ funciones.php
@@ -3,5 +3,9 @@ class claseFunciones{
 	function obtenerExtension($cadena) {
 		return substr(strrchr($cadena,'.'),1);
 	}
+  
+  	function log($cadena){
+      	return '<script>alert('.$cadena.')</script>';
+    }
 }
 ?>
