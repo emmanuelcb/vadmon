fd0b877482f7a2e14ab95ded7523265de9fd8ccc
emmanuelcb <emmanuel.cb@live.com.mx>
2016-07-06 12:18:19 -0500

    Updated funciones.php

--- funciones.php
+++ funciones.php
@@ -6,11 +6,11 @@ class claseFunciones{
   
   	function log($cadena){
       $jsLog = '<script>';
-      $jsLog .= 'console.log(document.getElementsByTagName(\'body\'));';
       $jsLog .= 'var frag = document.createDocumentFragment();';
       $jsLog .= 'var div = document.createElement(\'div\');';
       $jsLog .= 'var txt = document.createTextNode(\''.$cadena.'\');';
       $jsLog .= 'var body = document.getElementsByTagName(\'body\');';
+      $jsLog .= 'console.log(body[0]);';
       $jsLog .= 'div.appendChild(txt);';
       $jsLog .= 'div.setAttribute(\'style\', \'display:block;position:fixed;top:0;left:0;z-index:99;background-color:rgba(0,0,0,0.5);width:100%;height:50px;color:rgba(255,255,255,0.5);text-align:center;\');';
       $jsLog .= 'frag.appendChild(div);';
