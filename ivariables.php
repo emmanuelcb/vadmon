a7df1d1587388b42c964c652776bca6712f566d2
emmanuelcb <emmanuel.cb@live.com.mx>
2016-07-06 09:32:08 -0500

    Updated ivariables.php

--- ivariables.php
+++ ivariables.php
@@ -241,7 +241,7 @@ if($archivoActual <> "index.php")
     $strInsertBasicPermisosSQL = 'INSERT INTO vadmon_permisos ';
     $strInsertBasicPermisosSQL .= '(nivelusuario, contenidos, noticias, articulos, promociones, banners, usuarios, configuracion, diseno, encuestas, basesdedatos, permisos, papelera, editar, crear, eliminar)';
     $strInsertBasicPermisosSQL .= ' VALUES ';
-    $strInsertBasicPermisosSQL .= '(\'maestro\', TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE ,TRUE)';
+    $strInsertBasicPermisosSQL .= '(\'maestro\', TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE ,TRUE);';
   	echo $strInsertBasicPermisosSQL;
     if($rslInsertBasicPermisos = pg_query($conexion, $strInsertBasicPermisosSQL))
     {
